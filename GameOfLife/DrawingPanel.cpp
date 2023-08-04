dcbacbefc1920015e2c9ce1c017457daaacef7c9
Austin Miller <ALMiller4@student.fullsail.edu>
2023-08-04 03:42:44 -0600

    edited the on paint method to have the grid fill up more of the window

--- GameOfLife/DrawingPanel.cpp
+++ GameOfLife/DrawingPanel.cpp
@@ -2,15 +2,45 @@
 #include "wx/graphics.h"
 #include "wx/dcbuffer.h"
 
-DrawingPanel::DrawinngPanel(wxFrame* parent) : wxPanel(parent, wxID_ANY, wxPoint(0, 0), wxSize(200, 200))
+DrawingPanel::GraphicsPanel(wxFrame* parent) : wxPanel(parent, wxID_ANY, wxPoint(0, 0), wxSize(200, 200))
 {
 	SetBackgroundStyle(wxBG_STYLE_PAINT);
 	SetDoubeBuffered(true);
 
-	this->Bind(wxEVT_PAINT, &DrawingPanel::OnPaint, this)
+	this->Bind(wxEVT_PAINT, &DrawingPanel::OnPaint, this);
 }
 
-DrawingPanel::~DrawingPanel()
+DrawingPanel::~GraphicsPanel()
 {
 
+}
+
+void DrawingPanel::OnPaint(wxPaintEvent& event)
+{
+	wxAutoBufferedPaintDC dc(this);
+	dc.Clear();
+
+	wxGraphicsContext* context = wxGraphicsContext::Create(dc);
+	if (!context)
+	{
+
+	}return;
+
+	context->SetPen(*wxBLACK);
+	context->SetBrush(*wxWHITE);
+	wxSize size = GetSize();
+	int CellWidth = size.x / GridSize;
+	int CellHeight = size.y / GridSize;
+	int StartPointx = 0;
+	int StartPointy = 0;
+	for (int i = 0; i <GridSize ; i++)
+	{
+		
+		for (int j = 0; j < GridSize; j++)
+		{
+			context->DrawRectangle(StartPointx,StartPointy ,CellWidth,CellHeight);
+			StartPointx = CellWidth * 2 ;
+			StartPointy = CellHeight * 2 ;
+		}
+	}
 }
