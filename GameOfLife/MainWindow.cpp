402cfd0e6278f36d657653815f3e653338e4cd89
Austin Miller <ALMiller4@student.fullsail.edu>
2023-08-04 03:26:12 -0600

    created the drawing panel class and implemented creating a drawing pad and then making the inital grid

--- GameOfLife/MainWindow.cpp
+++ GameOfLife/MainWindow.cpp
@@ -3,12 +3,13 @@
 
 
  MainWindow::Window() : wxFrame(nullptr, wxID_ANY, "Game Of Life", wxPoint(0, 0), wxSize(300, 200))
-{
+ {
+	 DrawingPanel* Background = new DrawingPanel;
+ }
 
-}
  MainWindow::~Window()
  {
-
+	
  }
 
 
--- GameOfLife/MainWindow.h
+++ GameOfLife/MainWindow.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "wx/wx.h"
+#include "DrawingPanel.h"
 
 
 
 class MainWindow : public wxFrame
 {
-
+	DrawingPanel* Panel = nullptr;
 public:
-   Window();
-   ~Window();
+	Window();
+	~Window();
 };
